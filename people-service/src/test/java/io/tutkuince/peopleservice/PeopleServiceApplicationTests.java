915cde7626742a68590b7cd87b00690ca975e649
Tutku Ince <tutku.ince@outlook.com>
2024-01-31 20:42:34 +0300

    people service

---
+++ people-service/src/test/java/io/tutkuince/peopleservice/PeopleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tutkuince.peopleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
